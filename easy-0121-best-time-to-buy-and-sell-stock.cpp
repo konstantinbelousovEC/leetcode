4f50e510f6c176e75efd557f967d1089a1eb8c94
konstantinbelousovEC <kb_sd@yahoo.com>
2023-04-27 15:28:32 +0400

    best timr to buy and sell stock solution was added

---
+++ easy-0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    inline int maxProfit(vector<int>& prices) {
+        int min_price = 1000000;
+        int max_profit = 0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < min_price) {
+                min_price = prices[i];
+            } else if (prices[i] - min_price > max_profit) {
+                max_profit = prices[i] - min_price;
+            }
+        }
+        
+        return max_profit;
+    }
+};

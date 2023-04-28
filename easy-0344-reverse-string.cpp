6965d8758cabf24e9dab3baeb654cfcb4f604ace
konstantinbelousovEC <kb_sd@yahoo.com>
2023-04-28 20:50:32 +0400

    reverse of string solution was added

---
+++ easy-0344-reverse-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    inline void reverseString(vector<char>& s) {
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+};

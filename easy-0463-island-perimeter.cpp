1cec067465b0b1c700f8452daf98451c901210f0
konstantinbelousovEC <kb_sd@yahoo.com>
2023-07-11 16:30:51 +0400

    island perimeter solution was added

---
+++ easy-0463-island-perimeter.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+        int width = grid.front().size();
+        int height = grid.size();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) perimeter -= 2;
+                    if (j > 0 && grid[i][j - 1] == 1) perimeter -= 2;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};

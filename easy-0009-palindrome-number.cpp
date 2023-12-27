7fa079e7ba7444e4037c2870bd85b8be269601ee
konstantinbelousovEC <kb_sd@yahoo.com>
2023-12-27 22:09:01 +0300

    new solution of simple problem (is integer a palindrome)

--- easy-0009-palindrome-number.cpp
+++ easy-0009-palindrome-number.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
+// math
+
 #include <cassert>
 
 class Solution {
 public:
-	bool IsPolindrome(int x) {
-		if(x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        if (x < 10) return true;
+        
+        long long reversed_num = 0;
+        long long tmp_num = x;
+
+        while (tmp_num > 0) {
+            reversed_num *= 10;
+            reversed_num += tmp_num % 10;
+            tmp_num /= 10;
         }
 
-        int revertedNumber = 0;
-        while(x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
-            x /= 10;
-        }
-
-        return x == revertedNumber || x == revertedNumber/10;
-	}
+        return x == reversed_num;
+    }
 };
 
 int main() {
@@ -28,6 +31,7 @@ int main() {
 	assert(s.IsPolindrome(1));
 	assert(!s.IsPolindrome(111110));
 	assert(s.IsPolindrome(101202101));
+	assert(s.IsPolindrome(10122101));
 
 	return 0;
 }

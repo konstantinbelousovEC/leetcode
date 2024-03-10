9eeca78f9f28b24f579b4f98209987c8ee07022f
konstantinbelousovEC <kb_sd@yahoo.com>
2024-03-10 15:14:45 +0300

    corrections in palindrome number task

--- easy-0009-palindrome-number.cpp
+++ easy-0009-palindrome-number.cpp
@@ -1,37 +1,20 @@
-// math
-
-#include <cassert>
+// Math
+// https://leetcode.com/problems/palindrome-number/description/
 
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        if (x < 10) return true;
-        
-        long long reversed_num = 0;
-        long long tmp_num = x;
+        if (x < 0) return false;
+        if (x >= 0 && x < 10) return true;
 
-        while (tmp_num > 0) {
-            reversed_num *= 10;
-            reversed_num += tmp_num % 10;
-            tmp_num /= 10;
+        long long reversed_num = 0;
+        int x_copy = x;
+        while (x_copy != 0) {
+            reversed_num = (reversed_num * 10) + (x_copy % 10);
+            x_copy /= 10;
         }
 
         return x == reversed_num;
     }
 };
 
-int main() {
-	Solution s;
-
-	assert(s.IsPolindrome(121));
-	assert(!s.IsPolindrome(-121));
-	assert(s.IsPolindrome(12121));
-	assert(!s.IsPolindrome(120));
-	assert(s.IsPolindrome(1));
-	assert(!s.IsPolindrome(111110));
-	assert(s.IsPolindrome(101202101));
-	assert(s.IsPolindrome(10122101));
-
-	return 0;
-}
